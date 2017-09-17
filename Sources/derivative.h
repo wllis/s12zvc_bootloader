f17f2be93313e55ca0d197e014ef1f62e4f8de18
yxfvc <yxfvc@163.com>
2017-09-17 21:42:52 +0800

    123

--- Sources/derivative.h
+++ Sources/derivative.h
@@ -1,10 +1,11 @@
 /*
  * Note: This file is recreated by the project wizard whenever the MCU is
- *       changed and should not be edited by hand
- */
+ *      dfsf changed and should not be edited by hand
+ */                     
 
 /* Include the derivative-specific header file */
 #include <MC9S12XS128.h>
 
-#pragma LINK_INFO DERIVATIVE "MC9S12XS128"
+#pragma LINK_INFO DERIVATIVE "MC9S12XS128"  //asdf
 
+           
